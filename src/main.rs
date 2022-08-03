7e402bdd605172828c640c7f7063b18eeaf0c382
Hai Nghia <nghiahh.nta@gmail.com>
2022-08-03 11:39:23 +0700

    Secrect

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,29 @@
 mod routes;
 
+#[cfg(test)]
+mod test;
+
 #[macro_use]
 extern crate rocket;
 
-use rocket::{Build, Rocket};
+use rocket::{Build, Config, Rocket, State};
+use rocket::fairing::AdHoc;
+use rocket::serde::Deserialize;
 use crate::routes::{home::*, dynamic_path::*, ignored_segments::*, forwarding::*};
+
+
+#[derive(Debug, Deserialize)]
+#[serde(crate = "rocket::serde")]
+struct AppConfig {
+    key: String,
+    port: u16,
+}
+
+#[get("/")]
+fn read_config(rocket_config: &Config, app_config: &State<AppConfig>) -> String {
+    format!("{:#?}\n{:#?}", app_config, rocket_config)
+}
+
 
 #[launch]
 fn rocket() -> Rocket<Build> {
@@ -13,4 +32,6 @@ fn rocket() -> Rocket<Build> {
         .mount("/dynamic_path", routes![dynamic_path])
         .mount("/ignored", routes![foo_bar, everything])
         .mount("/forwarding", routes![user_usize, user_isize, user_str])
+        .mount("/config", routes![read_config])
+        .attach(AdHoc::config::<AppConfig>())
 }
--- src/routes.rs
+++ src/routes.rs
@@ -2,3 +2,4 @@ pub mod home;
 pub mod dynamic_path;
 pub mod ignored_segments;
 pub mod forwarding;
+pub mod request_guards;
